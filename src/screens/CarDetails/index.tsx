22c584cbcd817f8f3c60be2ae0143b306eafa608
Maria Clara <mariaclaracesar1_5@hotmail.com>
2022-05-12 18:41:40 -0300

    criando componente de botão

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg'
+import sccelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
 
 import {
   Container,
@@ -15,7 +24,9 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Accessories,
+  Footer
 } from './styles';
 
 export function CarDetails(){
@@ -44,10 +55,37 @@ export function CarDetails(){
           </Rent>
         </Details>
 
+        <Accessories>
+          <Accessory name='380Km/h' icon={speedSvg}/>
+          <Accessory name='3.2s' icon={sccelerationSvg}/>
+          <Accessory name='800 HP' icon={forceSvg}/>
+          <Accessory name='Gasolina' icon={gasolineSvg}/>
+          <Accessory name='Auto' icon={exchangeSvg}/>
+          <Accessory name='2 pessoas' icon={peopleSvg}/>
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
         </About>
       </Content>
+
+      <Footer>
+        <Button 
+          title='Confirmar' 
+        />
+      </Footer>
       
 
     </Container>
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
@@ -79,4 +79,23 @@ export const About = styled.Text`
   text-align: justify;
 
   margin-top: 23px;
+  line-height: ${RFValue(25)}px;
+`;
+
+export const Accessories = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 16px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+
+  padding: 24px 24px ${getBottomSpace() + 24}px;
 `;
