78ab4e47c4e8b06c0ebe6440626f8247d478197c
Maria Clara <mariaclaracesar1_5@hotmail.com>
2022-05-12 14:40:29 -0300

    criando componente de slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
-  Header
+  Header,
+  CarImages
 } from './styles';
 
 export function CarDetails(){
@@ -13,6 +15,13 @@ export function CarDetails(){
       <Header>
       <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={['https://img2.gratispng.com/20180201/jpq/kisspng-car-audi-christmas-jeep-wrangler-vehicle-audi-5a736b7e173691.4030715215175135980951.jpg']}
+        />
+      </CarImages>
+
     </Container>
   );
 }
