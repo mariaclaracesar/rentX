397bc37e8d0cd1897faf63e8a265a5a75ee024c2
Maria Clara <mariaclaracesar1_5@hotmail.com>
2022-05-16 17:57:40 -0300

    interface de agendamento

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 
-import { CarDetails } from './src/screens/CarDetails'
+import { Scheduling } from './src/screens/Scheduling'
 
 import {
   useFonts,
@@ -34,7 +34,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
   )
 }
